98d54a604a0536be61f49310a13f45bba3b02dfd
maliksaaad906-jpg <maliksaaad906@gmail.com>
2025-08-20 12:15:57 +1000

    Commit All

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// app/page.tsx
+import React from 'react';
+import BlogList from './Components/BlogList.tsx';
+
+const App = () => {
+  return (
+    <div>
+      <BlogList />
+    </div>
+  );
+};
+
+export default App;
